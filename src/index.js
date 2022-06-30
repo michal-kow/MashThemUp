a2f0451cbe4815d9a265ea3549d107c5d2acf640
Michał Kowalik <michal.t.kowalik@gmail.com>
2022-06-30 14:37:19 +0200

    routing problems

--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-(function(l) {
-  if (l.search[1] === '/' ) {
-    var decoded = l.search.slice(1).split('&').map(function(s) { 
-      return s.replace(/~and~/g, '&')
-    }).join('?');
-    window.history.replaceState(null, null,
-        l.pathname.slice(0, -1) + decoded + l.hash
-    );
-  }
-}(window.location))
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
