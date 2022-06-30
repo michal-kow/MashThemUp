939e95bd8809734bc55c86f8c6bda6eb1f7d4a19
Michał Kowalik <michal.t.kowalik@gmail.com>
2022-06-30 14:28:07 +0200

    fixing redirect problems

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+      <script type="text/javascript">
+        {(function(l) {
+          if (l.search[1] === '/' ) {
+            var decoded = l.search.slice(1).split('&').map(function(s) { 
+              return s.replace(/~and~/g, '&')
+            }).join('?');
+            window.history.replaceState(null, null,
+                l.pathname.slice(0, -1) + decoded + l.hash
+            );
+          }
+        }(window.location))}
+      </script>
       <App />
     </BrowserRouter>
   </React.StrictMode>
